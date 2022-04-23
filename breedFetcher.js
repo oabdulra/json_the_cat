7fd7daf16c4e0e67c69c1806d1c038d2db0cc2ec
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-04-23 10:59:46 -0400

    added an index file and refactored breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,33 @@
 const request = require('request'); 
-const breed = process.argv.slice(2);
-let apiSearch = {
-  url: `https://api.thecatapi.com/v1/breeds/search?q=${breed[0]}`,
-  headers: {
-    'x-api-key': '7e2cb31f-5f9d-4b4b-b872-dd2b2b7a2b9a'
-  }
+
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName[0]}`,  (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    
+    const data = JSON.parse(body);
+    if(!data.length) {
+      callback('Cat cannot be found', null);
+      return;
+    }
+        
+    
+    
+    callback(null, data[0].description);
+    
+      
+    
+   
+  });
+  
 };
 
-request(apiSearch,  (error, response, body) => {
-  if(!error) {
-  
-    if(body) {
-      const data = JSON.parse(body);
-      console.log(data[0].description);
-      
-    }
-  
-  } else {
-    console.error(error);
-  }
 
-});
 
+
+module.exports = { fetchBreedDescription };
